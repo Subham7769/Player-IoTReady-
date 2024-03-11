47d30d6b8b9e8f4539913d9b7e1ae59002abdad5
Subham jain <134865513+Subham7769@users.noreply.github.com>
2024-03-11 16:27:28 +0000

    updated code

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Player.css";
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
@@ -9,9 +9,11 @@ import ListItemText from '@mui/material/ListItemText';
 import IconButton from '@mui/material/IconButton';
 import PlayCircleFilledWhiteRoundedIcon from '@mui/icons-material/PlayCircleFilledWhiteRounded';
 
+
 const Player = () => {
   const [url, setUrl] = useState(null); // State to store the selected audio URL
   const [playlist, setPlaylist] = useState([]);
+  const audioRef = useRef(null); // Reference to the audio element
 
   const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -28,7 +30,7 @@ const Player = () => {
   // Function to handle file upload
   const handleChange = (e) => {
     const files = e.target.files;
-
+    console.log(url);
     Promise.all(Array.from(files).map(file => {
       return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -48,48 +50,67 @@ const Player = () => {
     });
   };
 
+  // Function to handle clicking on a playlist item
+  const handleClick = (url) => {
 
+    setUrl(null); // Set the URL
+    setTimeout(()=>{
+      setUrl(url)
+    console.log(url);
+    },20)
+    if (audioRef.current) {
+      audioRef.current.play(); // Play the audio
+    }
+  };
 
   return (
-    <div className="player">
-      <h3>Audio Player</h3>
-      <div className="uploadSection">
-        <Button
-          component="label"
-          role={undefined}
-          variant="contained"
-          tabIndex={-1}
-          startIcon={<CloudUploadIcon />}
-        >
-          Upload file
-          <VisuallyHiddenInput type="file" id="uploadedFile" accept=".mp3" multiple onChange={handleChange} />
-        </Button>
-      </div>
-      <div className="listSection">
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          {playlist.map((item, index) => (
-            <ListItem
-              key={index}
-              disableGutters
-              onClick={() =>  setUrl(item.url)} // Pass the URL to handleClick
-              secondaryAction={
-                <IconButton aria-label="play">
-                  <PlayCircleFilledWhiteRoundedIcon />
-                </IconButton>
-              }
-            >
-              <ListItemText primary={item.name} />
-            </ListItem>
-          ))}
-        </List>
+    <div className="playerBody">
+      
+      <div className="trackSection">
+        <div className="uploadSection">
+          <h4>Upload Track</h4>
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload file
+            <VisuallyHiddenInput type="file" id="uploadedFile" accept=".mp3" multiple onChange={handleChange} />
+          </Button>
+        </div>
+        <div className="listSection">
+          <h4>All Tracks</h4>
+          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {playlist.map((item, index) => (
+              <ListItem
+                key={index}
+                disableGutters
+                onClick={() => handleClick(item.url)} // Pass the URL to handleClick
+                secondaryAction={
+                  <IconButton aria-label="play">
+                    <PlayCircleFilledWhiteRoundedIcon />
+                  </IconButton>
+                }
+              >
+                <ListItemText primary={item.name} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </div>
       <div className="playerSection">
-        {url && ( // Render audio element only if URL is selected
-          <audio controls>
-            <source src={url} type="audio/mpeg" />
-            Your browser does not support the audio tag.
-          </audio>
-        )}
+        <div className="trackImage">
+        </div>
+        <div className="player">
+          {url && (
+            <audio ref={audioRef} controls autoPlay className="audioPlayer">
+              <source src={url} type="audio/mpeg" />
+              Your browser does not support the audio tag.
+            </audio>
+          )}
+        </div>
       </div>
     </div>
   );
