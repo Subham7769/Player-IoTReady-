107dd5b21a9ab0d7385796620cd48a56a2b57346
Shubham Jain <shubham.rapariya2@gmail.com>
2024-03-11 10:36:10 +0530

    UI created

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -4,10 +4,18 @@ import "./Player.css";
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import PlayCircleFilledWhiteRoundedIcon from '@mui/icons-material/PlayCircleFilledWhiteRounded';
 
 const Player = () => {
   
-  const [playlist, setPlaylist] = useState([]);
+  const [playlist, setPlaylist] = useState([{
+    name: 'playlist',
+    author:"shubham"
+  }]);
   const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
     clipPath: 'inset(50%)',
@@ -19,6 +27,9 @@ const Player = () => {
     whiteSpace: 'nowrap',
     width: 1,
   });
+  const handleChange = ()=>{
+    alert('Please select');
+  }
 
   return (
     <div className="player">
@@ -30,12 +41,29 @@ const Player = () => {
           variant="contained"
           tabIndex={-1}
           startIcon={<CloudUploadIcon />}
+          
         >
           Upload file
-          <VisuallyHiddenInput type="file" />
+          <VisuallyHiddenInput type="file" id="uploadedFile" onChange={handleChange}/>
         </Button>
       </div>
-      <div className="listSection"></div>
+      <div className="listSection">
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {playlist.map((item,index) => (
+        <ListItem
+          key={item+index}
+          disableGutters
+          secondaryAction={
+            <IconButton aria-label="play">
+              <PlayCircleFilledWhiteRoundedIcon />
+            </IconButton>
+          }
+        >
+          <ListItemText primary={item.name} />
+        </ListItem>
+      ))}
+    </List>
+      </div>
     </div>
   );
 };
